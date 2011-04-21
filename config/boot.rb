b75db5de84e9f6fe6c26a4891bb348b0229c8ebc
bretweinraub <bret@ckuru.com>
2011-04-21 17:50:01 +0000

    Add in the basics for the sm db build

---
+++ config/boot.rb
@@ -0,0 +1,16 @@
+
+require 'rubygems'
+require 'active_record'
+require 'log4r'
+include Log4r
+
+RAILS_ROOT = PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+CONFIG_ROOT = File.dirname(__FILE__)
+
+DB_CONFIG = YAML::load(File.open("#{CONFIG_ROOT}/database.yml"))
+ENVIRONMENT = ENV["RAILS_ENV"] || "development"
+ActiveRecord::Base.establish_connection(DB_CONFIG[ENVIRONMENT])
+
+require 'config/load_plugins'
+
+
